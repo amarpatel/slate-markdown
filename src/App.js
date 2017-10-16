e05351fe7db843a48f824ee223624d33d309c698
Amar Patel <theamarpatel@gmail.com>
2017-10-15 17:13:02 -0700

    Added copy fn to convert to raw markdown

--- src/App.js
+++ src/App.js
@@ -52,10 +52,22 @@ class App extends React.Component {
     this.setState({ state })
   }
 
+  convertToHtml(doc) {
+    return convertToHtml(doc);
+  }
+
+  onCopyHandler(e) {
+    const { state } = this.editor.state;
+    const { document } = state;
+    console.log('here: ', this.convertToHtml(document.toJS()));
+  }
+
   // Render the editor.
   render() {
     return (
       <Editor
+        ref={ (editor) => {this.editor = editor;} }
+        onCopy={(...args) => this.onCopyHandler(...args)}
         plugins={plugins}
         schema={this.state.schema}
         state={this.state.state}
@@ -66,3 +78,133 @@ class App extends React.Component {
 }
 
 export default App;
+
+function convertToHtml(state) {
+    let t = '';
+    if (state.nodes) {
+        state.nodes.forEach((node) => {
+            switch(node.type) {
+                case 'paragraph': {
+                    const text = convertToHtml(node);
+                    if (text.length) {
+                        t += `${text}\n`
+                    }
+                    break;
+                }
+                case 'bold': {
+                    const text = convertToHtml(node);
+                    if (text.length) {
+                        t += `**${text}**`
+                    }
+                    break;
+                }
+                case 'code': {
+                    const text = convertToHtml(node);
+                    if (text.length) {
+                        t += `\`${text}\``
+                    }
+                    break;
+                }
+                case 'underline': {
+                    const text = convertToHtml(node);
+                    if (text.length) {
+                        t += `_${text}_`
+                    }
+                    break;
+                }
+                case 'strike': {
+                    const text = convertToHtml(node);
+                    if (text.length) {
+                        t += `~~${text}~~`
+                    }
+                    break;
+                }
+                case 'block-quote': {
+                    const text = convertToHtml(node);
+                    if (text.length) {
+                        t += `\n> ${text}\n`;
+                    }
+                    break;
+                }
+                case 'bulleted-list': {
+                    const text = convertToHtml(node);
+                    if (text.length) {
+                        t += `\n- ${text}\n`;
+                    }
+                    break;
+                }
+                case 'heading-one': {
+                    const text = convertToHtml(node);
+                    if (text.length) {
+                        t += `\n# ${text}\n`;
+                    }
+                    break;
+                }
+                case 'heading-two': {
+                    const text = convertToHtml(node);
+                    if (text.length) {
+                        t += `\n## ${text}\n`;
+                    }
+                    break;
+                }
+                case 'heading-three': {
+                    const text = convertToHtml(node);
+                    if (text.length) {
+                        t += `\n### ${text}\n`;
+                    }
+                    break;
+                }
+                case 'heading-four': {
+                    const text = convertToHtml(node);
+                    if (text.length) {
+                        t += `\n#### ${text}\n`;
+                    }
+                    break;
+                }
+                case 'heading-five': {
+                    const text = convertToHtml(node);
+                    if (text.length) {
+                        t += `\n##### ${text}\n`;
+                    }
+                    break;
+                }
+                case 'heading-six': {
+                    const text = convertToHtml(node);
+                    if (text.length) {
+                        t += `\n###### ${text}\n`;
+                    }
+                    break;
+                }
+                case 'list-item': {
+                    const text = convertToHtml(node);
+                    if (text.length) {
+                        t += `\n+ ${text}\n`;
+                    }
+                    break;
+                }
+                case 'code-block': {
+                    const text = convertToHtml(node);
+                    if (text.length){
+                        t += `\n\`\`\`${text}\`\`\``;
+                    }
+                    break;
+                }
+                default: {
+                    console.log('in default:type: ', node.type);
+                    console.log('in default:kind: ', node.kind);
+                    t += convertToHtml(node);
+                }
+            }
+        });
+    }
+
+    if (state.ranges) {
+        state.ranges.forEach((range) => {
+            if (range.text.length) {
+                t += range.text
+            }
+        });
+    }
+
+    return t;
+}
